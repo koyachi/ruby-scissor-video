50f7ea6a0e155c4711e1d9b4f07fc4d198b524ed
koyachi <rtk2106@gmail.com>
2009-07-05 17:37:17 +0900

    mv video.rb chunk.rb

--- lib/scissor-video.rb
+++ lib/scissor-video.rb
@@ -1,7 +1,7 @@
 require 'scissor'
 require 'scissor-video/float'
 require 'scissor-video/command'
-require 'scissor-video/video'
+require 'scissor-video/chunk'
 require 'scissor-video/video_file'
 
 module Scissor
